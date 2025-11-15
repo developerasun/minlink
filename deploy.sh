b46658399d6345de3a63691e2512be152468dfe1
developerasun <nellow1102@gmail.com>
2025-11-15 17:24:11 +0900

    refactor: update deploying script for minlink

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker build -t hexbook-backend:latest .
-docker service rm hexbook_backend
-docker stack up -c ./service.yaml hexbook
+docker build -t minlink-backend:latest .
+docker service rm minlink_backend
+docker stack up -c ./service.yaml minlink
 docker service ls
